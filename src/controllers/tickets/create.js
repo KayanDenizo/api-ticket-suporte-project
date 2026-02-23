5004277891280f64f01b32bfaafba120aac0932d
kayandenizo <kayan.denizo@gmail.com>
2026-02-23 18:08:25 -0300

    Feat: Enhance ticket creation by generating a unique ID and including additional ticket details in the response

--- src/controllers/tickets/create.js
+++ src/controllers/tickets/create.js
@@ -1,6 +1,19 @@
+import { randomUUID } from "node:crypto"
 export function create({ request, response }) {
     // passar parametros entre chavez nao importa a ordem dos parametros, apenas uma questao de prevenção
+    const { equipment, description, user_name } = request.body
 
-    return response.end("Criado com Sucesso")
+    const ticket = {
+        id: randomUUID(), // gera um id aleatorio
+        equipment, 
+        description, 
+        user_name, // nome do usuario que esta abrindo o ticket
+        status: "open",
+        created_at: new Date(), // data de criacao do ticket
+        updated_at: new Date(), // data de atualizacao do ticket
+    }
+
+    return response.end(JSON.stringify(ticket))
+
 
 }
